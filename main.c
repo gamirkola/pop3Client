ffb5e3dae452998edfe4a64274e47b9541d250c0
gamirkola <mirkogal@gmail.com>
2019-04-04 17:55:46 +0200

    modifica exit(-1)

--- main.c
+++ main.c
@@ -8,23 +8,25 @@
 #include <netdb.h>
 
 
-int Invia(int sock, char *comando[]){
+void Invia(int sock, char *comando[]){
     printf(comando);
     if(send(sock,comando,strlen(comando),0)<0){
-        return -1;
+        printf ("Errore di invio\n");
+        exit(-1);
     }
-    return 2;
+    return;
 }
 
-int Ricevi(int sock){
+void Ricevi(int sock){
     char *buf[4096] = { 0 };
 
-    if ( recv (sock, buf, 4096, 0) == -1){
-       return -1;
-     }
+    if ( recv (sock, buf, 4096, 0)<0){
+        printf ("Errore di ricezione\n");
+        exit(-1);
+    }
     printf(buf);
 
-    return 2;
+    return;
 }
 
 int main(int argc,char* argv[])
@@ -47,7 +49,7 @@ int main(int argc,char* argv[])
   if (h==0)
   {
     printf("Gethostbyname fallito\n");
-    exit(1);
+    exit(-1);
   }
   bcopy(h->h_addr,&temp.sin_addr,h->h_length);
   //Creazione socket.
@@ -55,28 +57,23 @@ int main(int argc,char* argv[])
   //Connessione del socket. Esaminare errore per compiere azioni
   //opportune in caso di errore.
   errore=connect(sock, (struct sockaddr*) &temp, sizeof(temp));
-  if(Ricevi(sock)==1){
-    printf("errore di ricezione\n");
-  }
+  Ricevi(sock);
 
   while(1){
 
     printf("Inserisci un comando: ");
-    char str[20];
-    fgets(str, 20, stdin);
+    fgets(msg, 4096, stdin);
 
-    if(strcmp(str,"esci")){
-        exit(1);
+    if(strcmp(msg,"esci")){
+        exit(-1);
     }
 
-    if(Invia(sock,str)==-1){
+     if(send(sock,msg,strlen(msg),0)<0){
         printf ("Errore di invio\n");
-        exit(1);
+        exit(-1);
     }
-    if(Ricevi(sock)==-1){
-        printf ("Errore di ricezione\n");
-        exit(1);
-    }
+    printf(msg);
+    Ricevi(sock);
 
   }
 
